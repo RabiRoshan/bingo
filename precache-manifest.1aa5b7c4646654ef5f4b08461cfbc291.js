1aa46b241cff975faee0881302f7e21cff368d99
Rabi Roshan <rabiroshankottampara@gmail.com>
2020-06-15 15:23:44 +0530

    Updates

---
+++ precache-manifest.1aa5b7c4646654ef5f4b08461cfbc291.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2df26412e6ca69c93ac39051ec318d8c",
+    "url": "/bingo/index.html"
+  },
+  {
+    "revision": "1c205b0a8ae52b74101e",
+    "url": "/bingo/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "d81b3cb1e40c3f346a03",
+    "url": "/bingo/static/js/2.f1fb9d1f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bingo/static/js/2.f1fb9d1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c205b0a8ae52b74101e",
+    "url": "/bingo/static/js/main.479d2b30.chunk.js"
+  },
+  {
+    "revision": "3e96d5cca2553c021ce2",
+    "url": "/bingo/static/js/runtime-main.d558581a.js"
+  }
+]);
